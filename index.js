c14d1b40190c51beaf071797b0d0b781f74714fd
hirorich <ch.dim7ir@gmail.com>
2020-11-23 15:54:19 +0900

    共通部品へのリンク記載

---
+++ index.js
@@ -0,0 +1,9 @@
+// 定義したコンポーネントを登録
+const vm = new Vue({
+    el: "#main-component",
+    template: `
+        <div>
+            <a href="https://github.com/hirorich/PagesCommon#readme">README.md</a>
+        </div>
+        `,
+});
